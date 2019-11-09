887e9669c14c0daebf96e2599af14e3bfe730926
shakthisachintha <43114582+shakthisachintha@users.noreply.github.com>
2019-11-09 07:13:13 +0530

    mailing function set

--- app/Mail/SendGridMail.php
+++ app/Mail/SendGridMail.php
@@ -11,23 +11,25 @@ class SendGridMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $data;
+
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
-        return $this->view('view.name');
+        $address = 'janeexampexample@example.com';
+        $subject = 'This is a demo!';
+        $name = 'Jane Doe';
+        
+        return $this->view('emails.test')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject)
+                    ->with([ 'message' => $this->data['message'] ]);
     }
 }
